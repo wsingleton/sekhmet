498904b1c600ac598e5cd329ee6b5b4f26d23636
wsingleton <wezley.singleton@gmail.com>
2019-07-31 20:57:02 -0400

    refactored Address.hashcode and .equals to use Java 8 features

--- sekhmet-api/src/main/java/dev/innov8/sekhmet/models/Address.java
+++ sekhmet-api/src/main/java/dev/innov8/sekhmet/models/Address.java
@@ -1,5 +1,7 @@
 package dev.innov8.sekhmet.models;
 
+import java.util.Objects;
+
 public class Address {
 	
 	private String unit;
@@ -63,14 +65,7 @@ public class Address {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((city == null) ? 0 : city.hashCode());
-		result = prime * result + ((state == null) ? 0 : state.hashCode());
-		result = prime * result + ((street == null) ? 0 : street.hashCode());
-		result = prime * result + ((unit == null) ? 0 : unit.hashCode());
-		result = prime * result + ((zipcode == null) ? 0 : zipcode.hashCode());
-		return result;
+		return Objects.hash(city, state, street, unit, zipcode);
 	}
 
 	@Override
@@ -79,35 +74,12 @@ public class Address {
 			return true;
 		if (obj == null)
 			return false;
-		if (getClass() != obj.getClass())
+		if (!(obj instanceof Address))
 			return false;
 		Address other = (Address) obj;
-		if (city == null) {
-			if (other.city != null)
-				return false;
-		} else if (!city.equals(other.city))
-			return false;
-		if (state == null) {
-			if (other.state != null)
-				return false;
-		} else if (!state.equals(other.state))
-			return false;
-		if (street == null) {
-			if (other.street != null)
-				return false;
-		} else if (!street.equals(other.street))
-			return false;
-		if (unit == null) {
-			if (other.unit != null)
-				return false;
-		} else if (!unit.equals(other.unit))
-			return false;
-		if (zipcode == null) {
-			if (other.zipcode != null)
-				return false;
-		} else if (!zipcode.equals(other.zipcode))
-			return false;
-		return true;
+		return Objects.equals(city, other.city) && Objects.equals(state, other.state)
+				&& Objects.equals(street, other.street) && Objects.equals(unit, other.unit)
+				&& Objects.equals(zipcode, other.zipcode);
 	}
 
 	@Override
