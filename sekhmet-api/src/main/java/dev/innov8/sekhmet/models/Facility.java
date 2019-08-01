0e09871e78f43ce4cc4389a887c827a5a5dc61c6
wsingleton <wezley.singleton@gmail.com>
2019-07-31 20:54:06 -0400

    added address field to facility model

--- sekhmet-api/src/main/java/dev/innov8/sekhmet/models/Facility.java
+++ sekhmet-api/src/main/java/dev/innov8/sekhmet/models/Facility.java
@@ -7,6 +7,7 @@ public class Facility {
 	
 	private int id;
 	private String name;
+	private Address address;
 	private Customer owner;
 	private PointOfContact contact;
 	private List<Room> rooms;
@@ -15,18 +16,20 @@ public class Facility {
 		super();
 	}
 
-	public Facility(int id, String name, Customer owner, PointOfContact contact) {
+	public Facility(int id, String name, Address address, Customer owner, PointOfContact contact) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.address = address;
 		this.owner = owner;
 		this.contact = contact;
 	}
 
-	public Facility(int id, String name, Customer owner, PointOfContact contact, List<Room> rooms) {
+	public Facility(int id, String name, Address address, Customer owner, PointOfContact contact, List<Room> rooms) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.address = address;
 		this.owner = owner;
 		this.contact = contact;
 		this.rooms = rooms;
@@ -46,6 +49,14 @@ public class Facility {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
 	}
 
 	public Customer getOwner() {
@@ -74,7 +85,7 @@ public class Facility {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(contact, id, name, owner, rooms);
+		return Objects.hash(address, contact, id, name, owner, rooms);
 	}
 
 	@Override
@@ -86,13 +97,16 @@ public class Facility {
 		if (!(obj instanceof Facility))
 			return false;
 		Facility other = (Facility) obj;
-		return Objects.equals(contact, other.contact) && id == other.id && Objects.equals(name, other.name)
-				&& Objects.equals(owner, other.owner) && Objects.equals(rooms, other.rooms);
+		return Objects.equals(address, other.address) && Objects.equals(contact, other.contact) && id == other.id
+				&& Objects.equals(name, other.name) && Objects.equals(owner, other.owner)
+				&& Objects.equals(rooms, other.rooms);
 	}
 
 	@Override
 	public String toString() {
-		return "Facility [id=" + id + ", name=" + name + ", owner=" + owner + ", contact=" + contact + "]";
+		return "Facility [id=" + id + ", name=" + name + ", address=" + address + ", owner=" + owner + ", contact="
+				+ contact + ", rooms=" + rooms + "]";
 	}
+
 }
 
