4223be9a30248516cf06f30f4d7742ab4a7905d1
wsingleton <wezley.singleton@gmail.com>
2019-08-03 14:26:56 -0400

    added Transactional annotations to RegisterService methods

--- sekhmet-api/src/main/java/dev/innov8/sekhmet/services/RegisterService.java
+++ sekhmet-api/src/main/java/dev/innov8/sekhmet/services/RegisterService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.innov8.sekhmet.models.Register;
 import dev.innov8.sekhmet.repositories.RegisterRepository;
@@ -19,20 +20,24 @@ public class RegisterService {
 		this.registerRepo = repo;
 	}
 	
+	@Transactional(readOnly=true)
 	public List<Register> getAllRegisters() {
 		return registerRepo.findAll();
 	}
 	
+	@Transactional(readOnly=true)
 	public List<Register> getRegistersByType(String type) {
 		return registerRepo.findByType(type);
 	}
 	
+	@Transactional(readOnly=true)
 	public Register getRegisterById(int id) {
 		Optional<Register> _register = registerRepo.findById(id);
 		if(_register.isPresent()) return _register.get();
 		else throw new RuntimeException("No register with id, " + id + " found!");
 	}
 	
+	@Transactional
 	public Register upsertRegister(Register register) {
 		return registerRepo.save(register);
 	}
