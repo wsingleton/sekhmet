36b244c106b1343f17f04942af3aeee52e413cec
wsingleton <wezley.singleton@gmail.com>
2019-08-03 16:25:43 -0400

    added Swagger API documentation to project

---
+++ sekhmet-api/src/main/java/dev/innov8/sekhmet/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package dev.innov8.sekhmet.config;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	  
+	private final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+	  
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+			.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+			.consumes(DEFAULT_PRODUCES_AND_CONSUMES)
+			.select()
+			.apis(RequestHandlerSelectors.any())
+			.paths(PathSelectors.any())
+			.build();
+	}
+	
+}
